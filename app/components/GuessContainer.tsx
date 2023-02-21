8f1df08b8a9f0e1cf53e9a2bd5de80356166fe35
Adam <adam.shelley18@gmail.com>
2023-02-21 16:31:41 +0800

    Fixing missing prop

--- app/components/GuessContainer.tsx
+++ app/components/GuessContainer.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { checkLocalStorage } from "../utils/checkLocalStorage";
 import GuessCard from "./GuessCard";
 import SubmitButton from "./SubmitButton";
 import SubmitSuccess from "./SubmitSuccess";
+
+const fetchLetters = async () => {
+  const response = await axios.get("/api/highscore/getLetters");
+  return response.data;
+};
 
 export default function GuessContainer() {
   const [word, setWord] = useState("");
@@ -19,12 +26,19 @@ export default function GuessContainer() {
 
   if (successSubmittedResult) return <SubmitSuccess />;
 
+  const { data, isLoading } = useQuery({
+    queryFn: fetchLetters,
+    queryKey: ["get-letters"],
+  });
+
+  if (isLoading) return <p>loading..</p>;
+
   return (
     <>
       {!successSubmittedResult && (
         <div className="flex flex-col">
           <div className="flex justify-center align-center py-2 mt-5">
-            <GuessCard word={word} setWord={setWord} />
+            <GuessCard letters={data?.letters} word={word} setWord={setWord} />
           </div>
           <SubmitButton completeWord={word} setWord={setWord} />
         </div>
